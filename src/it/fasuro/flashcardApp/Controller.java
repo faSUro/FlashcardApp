b6efdf32cc03547a7b39861ec8c98e7c6cefa405
fasu <fasulo.nicol@gmail.com>
2020-04-03 10:40:02 +0200

    completed the main frame graphic

---
+++ src/it/fasuro/flashcardApp/Controller.java
@@ -0,0 +1,5 @@
+package it.fasuro.flashcardApp;
+
+public class Controller {
+
+}
--- src/it/fasuro/flashcardApp/view/MainFrame.java
+++ src/it/fasuro/flashcardApp/view/MainFrame.java
@@ -1,17 +1,66 @@
 package it.fasuro.flashcardApp.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 	
-	public static int WIDTH = 500;
-	public static int HEIGHT = 500;
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 750;
+	
+	private JLabel questionLabel;
+	private JButton showAnswerButton;
+	private JButton easyButton;
+	private JButton mediumButton;
+	private JButton hardButton;
 	
 	public MainFrame() {
 		setTitle("FlashcardApp");
 		setSize(WIDTH, HEIGHT);
 		setLocationRelativeTo(null);
+		
+		JPanel mainPanel = new JPanel();
+		Container contentPane = getContentPane();
+		contentPane.add(mainPanel);
+		
+		mainPanel.setLayout(new BorderLayout());
+		
+		JPanel questionPanel = new JPanel();
+		questionLabel = new JLabel("Let's start!");
+		questionLabel.setFont(new Font("Dialog", Font.BOLD, 18));
+		questionPanel.add(questionLabel);
+		
+		JPanel answerPanel = new JPanel();
+		answerPanel.setLayout(new BorderLayout());
+		showAnswerButton = new JButton("  Start  ");
+		showAnswerButton.setFont(new Font("Dialog", Font.BOLD, 18));
+		showAnswerButton.setBackground(Color.WHITE);
+		answerPanel.add(showAnswerButton, BorderLayout.CENTER);
+		
+		JPanel difficultyPanel = new JPanel();
+		difficultyPanel.setLayout(new GridLayout(1, 3));
+		easyButton = new JButton(" Easy ");
+		easyButton.setFont(new Font("Dialog", Font.BOLD, 18));
+		mediumButton = new JButton(" Medium ");
+		mediumButton.setFont(new Font("Dialog", Font.BOLD, 18));
+		hardButton = new JButton(" Hard ");
+		hardButton.setFont(new Font("Dialog", Font.BOLD, 18));
+		difficultyPanel.add(easyButton);
+		difficultyPanel.add(mediumButton);
+		difficultyPanel.add(hardButton);
+		
+		mainPanel.add(questionPanel, BorderLayout.NORTH);
+		mainPanel.add(answerPanel, BorderLayout.CENTER);
+		mainPanel.add(difficultyPanel, BorderLayout.SOUTH);
 		
 		setVisible(true);
 	}
