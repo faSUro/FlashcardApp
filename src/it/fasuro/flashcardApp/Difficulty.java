20ca9f96a3868ccfc75d7071453d202dbe3d8199
fasu <fasulo.nicol@gmail.com>
2020-05-10 16:57:22 +0200

    added documentation

--- src/it/fasuro/flashcardApp/Difficulty.java
+++ src/it/fasuro/flashcardApp/Difficulty.java
@@ -1,5 +1,12 @@
 package it.fasuro.flashcardApp;
 
+/**
+ * Enumeration to decide whether the user found the
+ * question easy or quite difficult (very difficult,
+ * aka HARD, is not necessary).
+ * @author fasu
+ *
+ */
 public enum Difficulty {
 	
 	EASY, MEDIUM
